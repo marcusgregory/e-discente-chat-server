40e6c1e3685d41f1232186f8ca10f3d49d2398fc
Marcus Gregory <dev.marcusgregory@gmail.com>
2022-04-12 08:16:44 -0300

    Update messages.controller.ts

--- src/controllers/messages.controller.ts
+++ src/controllers/messages.controller.ts
@@ -29,7 +29,7 @@ class MessagesController extends BaseController {
             }
 
             if(gid){
-                var docs = await MessageModel.find({gid:gid}).sort({_id:1}).limit(perPage).skip(page*perPage);
+                var docs = await MessageModel.find({gid:gid}).sort({_id:-1}).limit(perPage).skip(page*perPage);
                 console.log('Limit: '+perPage+' Skip: '+page*perPage+' Page: '+page);
                 // docs.map((doc:any) => {
                 //     var data:Date = doc.get('sendAt');
